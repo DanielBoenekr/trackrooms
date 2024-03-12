c0fb62d2b71b4887085fd1aa40656f145769690e
justArtus <artus.kotowski@bayer.com>
2024-03-12 12:43:36 +0100

    Chenaged Footer.jjs no text for icons and added with and height

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,8 +6,8 @@ function Footer(){
                 <li><a href="impressum.html">Impressum</a></li>
             </ul>
             <div class="social-icons">
-                <a href="https://www.youtube.com/" target="_blank"><img src="youtube-icon.png" alt="YouTube" /></a>
-                <a href="https://www.instagram.com/" target="_blank"><img src="instagram-icon.png" alt="Instagram" /></a>
+                <a href="https://www.youtube.com/" target="_blank"><img src="youtube-icon.png" width="50" height="50"/></a>
+                <a href="https://www.instagram.com/" target="_blank"><img src="instagram-icon.png" width="50" height="50"/></a>
             </div>
         </div>
     )
