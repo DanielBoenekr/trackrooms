e49c9fdafa7b2bf1d50534463b6a1567f5227182
AaronAdvintoPriv <aadviento02@gmail.com>
2024-03-12 12:05:40 +0100

    added dropdown to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './DropDown.js';
+import DropDown from './dropdown/DropDown.js';
 
 //push these nuts
 
 function App() { 
   return (
-    <div className="App">
+    <div>
+      <DropDown 
+        id="1" 
+        labelText="My Profile" 
+        open={openAccordionId === "1"}
+        onClick={handleAccordionClick}
+      ></DropDown>
     </div>
   );
 }
