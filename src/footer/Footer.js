9ad0764c3422ad5b5bf82bc0a6173725fe2816d1
bayArtus <artus.kotowski@bayer.com>
2024-03-12 12:01:38 +0100

    Changed Footer.js and Footer.css

---
+++ src/footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <div class="container">
+            <ul>
+                <li><a href="datenschutz.html">Datenschutz</a></li>
+                <li><a href="impressum.html">Impressum</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer;
