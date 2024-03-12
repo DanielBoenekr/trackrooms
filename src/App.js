8755ef7f40f0cf2c7b9bd46f8bbca461c8969e53
DanielBoenekr <s_boeneker22@stud.hwr-berlin.de>
2024-03-12 11:56:42 +0100

    Header Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstPage from './firstPage/FirstPage';
 
 //push these nuts
 
 function App() { 
   return (
     <div className="App">
+    <FirstPage/>
     </div>
   );
 }
---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import Logo from './logo.png'
+
+function Header(){
+    return(
+        <div>
+        <img src="./logo.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""></img>
+        </div>
+    );
+}
+
+export default Header;
+
